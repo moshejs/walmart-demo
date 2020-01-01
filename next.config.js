59408537d2ede736f6e5fcad628475dd1fbc9fb4
Moshe <moshemalka@mail.adelphi.edu>
2019-12-31 23:54:56 -0500

    add current build

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ const withFonts = require('nextjs-fonts');
 
 module.exports =  withFonts(withSass({
   assetPrefix: process.env.NODE_ENV === 'production' ? 'https://moshejs.github.io/walmart-demo/' : '',
+  generateBuildId: async () => 'current',
 }))
 
