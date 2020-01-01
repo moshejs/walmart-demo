47a734eb9c2e00e3b96bc240c2c466926596fba5
Moshe <moshemalka@mail.adelphi.edu>
2020-01-01 02:03:04 -0500

    undo

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -16,9 +16,9 @@ const Product = ({ product }) => (
       <div className="itemName">{product.name}</div>
       <div className="originalPrice">{product.original_price}</div>
     </div>
-    <div className="container">
+    <div className="priceContainer">
       <div className="itemDescription">{product.description}</div>
-      <div className="priceContainer">
+      <div className="price">
         <span className={product.original_price ? "discountPrice" : "price"}>
           {product.price}
         </span>
