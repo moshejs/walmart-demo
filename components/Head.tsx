5086a53ad3d6aec1e6c228e74e01318db0b364ec
Moshe <moshemalka@mail.adelphi.edu>
2020-01-01 02:21:24 -0500

    fix scale mobile

--- components/Head.tsx
+++ components/Head.tsx
@@ -7,7 +7,7 @@ export default () => {
   return (
     <Head>
       <title>{siteTitle}</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" user-scalable="no" />
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css"
