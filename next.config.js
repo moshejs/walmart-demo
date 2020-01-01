840509f151a596c9e21e22c8358c61ebe0e982cb
Moshe <moshemalka@mail.adelphi.edu>
2019-12-31 23:06:21 -0500

    up

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withSass = require("@zeit/next-sass");
 const withFonts = require('nextjs-fonts');
-console.log(process.env.NODE_ENV)
+
 module.exports =  withFonts(withSass({
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/walmart-demo' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://moshejs.github.io/walmart-demo/' : '',
 }))
 
